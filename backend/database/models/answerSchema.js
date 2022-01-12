186e81b48b4bfc1fbca91a4757279c6ae0f6e7a4
majdinazzal <majdiahmadnazzal@gmail.com>
2022-01-13 00:14:10 +0200

    debug rout problem

--- backend/database/models/answerSchema.js
+++ backend/database/models/answerSchema.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const answerSchema = new mongoose.Schema({
   answer: { type: String },
-  student: { type: mongoose.Types.ObjectId, ref: "User" },
+  student: { type: mongoose.Types.ObjectId, ref: "User" } ,
 });
 
 module.exports = mongoose.model("Answer", answerSchema);
