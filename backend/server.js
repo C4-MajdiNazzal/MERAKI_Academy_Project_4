fd950174d8b749c45020c13df73b3afbfcd0b8e2
majdinazzal <majdiahmadnazzal@gmail.com>
2022-01-12 11:39:43 +0200

    putting routes in server

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+require("./db/db");
+
 const app = express();
-const db = require("./database/db");
-
-app.use(cors());
-
-app.use(express.json());
-app.use()
 const PORT = 5000;
 
+// Import Routers
+const articlesRouter = require("./routes/articles");
+const usersRouter = require("./routes/users");
+const loginRouter = require("./routes/login");
+const rolesRouter = require("./routes/roles");
+
+app.use(cors());
+app.use(express.json());
+
+// Routes Middleware
+app.use("/articles", articlesRouter);
+app.use("/users", usersRouter);
+app.use("/roles", rolesRouter);
+app.use("/login", loginRouter);
+
+// Handles any other endpoints [unassigned - endpoints]
+app.use("*", (req, res) => res.status(404).json("NO content at this path"));
+
 app.listen(PORT, () => {
-  console.log(`SERVER WORKING ON PORT: ${PORT}`);
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
