cf7acd50817442087b1647d02ddfb1231192f171
majdinazzal <majdiahmadnazzal@gmail.com>
2022-01-12 11:31:05 +0200

    creating questions  route

---
+++ backend/routes/questions.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+// Import questions controllers
+const {
+  getAllQuestions,
+  getQuestionsByAuthor,
+  getQuestionById,
+  createNewQuestion,
+  updateQuestionById,
+  deleteQuestionById,
+  deleteQuestionsByAuthor,
+} = require("../controllers/questions");
+
+// Import comments controller
+const { createNewComment } = require("./../controllers/comments");
+
+// Middleware
+const authentication = require("../middleware/authentication");
+const authorization = require("../middleware/authorization");
+
+// Create questions router
+const questionsRouter = express.Router();
+
+
+
+questionsRouter.get("/",authentication, getAllQuestions);
+questionsRouter.get("/search_1", getQuestionsByAuthor);
+questionsRouter.get("/search_2", getQuestionById);
+questionsRouter.post("/",authentication, createNewQuestion);
+questionsRouter.put("/:id", updateQuestionById);
+questionsRouter.delete("/:id", deleteQuestionById);
+questionsRouter.delete("/", deleteQuestionsByAuthor);
+
+questionsRouter.post(
+  "/:id/comments",
+  authentication,
+  authorization("CREATE_COMMENTS"),
+  createNewComment
+);
+
+module.exports = questionsRouter;
