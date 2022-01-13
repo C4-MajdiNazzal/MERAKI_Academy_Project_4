a14ed960cbb8a8dfe5716ba73e2cc4742aa86bc6
majdinazzal <majdiahmadnazzal@gmail.com>
2022-01-13 23:01:26 +0200

    string change in names

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -6,7 +6,7 @@ const authentication = (req, res, next) => {
     if (!req.headers.authorization) {
       return res.status(403).json({
         success: false,
-        message: `Forbidden`,
+        message: `Forbidden not authorized`,
       });
     }
     const token = req.headers.authorization.split(" ").pop();
