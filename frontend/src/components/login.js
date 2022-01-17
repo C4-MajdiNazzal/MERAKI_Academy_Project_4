bbaa78320308808a11144edea98f299f871a85c9
majdinazzal <majdiahmadnazzal@gmail.com>
2022-01-17 07:28:39 +0200

    components

---
+++ frontend/src/components/login.js
@@ -0,0 +1,82 @@
+import React, { useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import axios from "axios";
+
+import { AuthContext } from "../context/auth";
+
+//===============================================================
+
+const Login = () => {
+  const { setIsLoggedIn, isLoggedIn, saveToken } = useContext(AuthContext);
+  const history = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [password, setPassword] = useState("");
+  const [status, setStatus] = useState(false);
+
+  //===============================================================
+
+  const login = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("http://localhost:5000/login", {
+        email,
+        password,
+      });
+      if (res.data.success) {
+        setMessage("");
+        saveToken(res.data.token);
+        setIsLoggedIn(true);
+      } else throw Error;
+    } catch (error) {
+      if (error.response && error.response.data) {
+        return setMessage(error.response.data.message);
+      }
+      setMessage("Error happened while Login, please try again");
+    }
+  };
+
+  //===============================================================
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      history("/dashboard");
+    }
+  });
+
+  //===============================================================
+
+  return (
+    <>
+      <div className="Form">
+        <p className="Title">Login:</p>
+        <form onSubmit={login}>
+          <br />
+
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <button>Login</button>
+        </form>
+
+        {status
+          ? message && <div className="SuccessMessage">{message}</div>
+          : message && <div className="ErrorMessage">{message}</div>}
+      </div>
+    </>
+  );
+};
+
+export default Login;
