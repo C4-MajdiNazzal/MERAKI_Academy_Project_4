90cd84621443aa000b3abfbe41d089e96f7a219a
majdinazzal <majdiahmadnazzal@gmail.com>
2022-01-17 07:59:41 +0200

    react-router-dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// 
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+
+//context providers
+import AuthProvider from "./context/auth";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Router>,
+  document.getElementById("root")
 );
