e1597f16581dae6cdb9e394525a282bbad15817d
majdinazzal <majdiahmadnazzal@gmail.com>
2022-01-20 18:55:51 +0200

    AllQuestions

--- frontend/src/components/allQuestion.js
+++ frontend/src/components/allQuestion.js
@@ -2,17 +2,20 @@ import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-
 import { AuthContext } from "../context/auth";
 
 const AllQuestion = () => {
   const { token } = useContext(AuthContext);
   const [userId, setUserId] = useState("");
+  // const { token} = useContext(AuthContext);
   const [question, setQuestion] = useState("");
+  
   const [show, setShow] = useState(false);
   const [message, setMessage] = useState("");
   //====================================================================================
-
+  useEffect(() => {
+    getAllQuestions();
+  }, []);
   const getAllQuestions = async () => {
     try {
       const res = await axios.get("http://localhost:5000/questions", {
@@ -20,11 +23,13 @@ const AllQuestion = () => {
           Authorization: `Bearer ${token}`,
         },
       });
+      console.log("res.data", res.data.question);
       if (res.data.success) {
-        setQuestion(res.data.questions);
-        console.log(res.data.questions);
-        setMessage("");
-        setShow(true);
+        setQuestion(res.data.question);
+        console.log(question);
+        console.log(res.data.question);
+        // setMessage(res.data,message);
+        // setShow(true);
         setUserId(res.data.userId);
       } else throw Error;
     } catch (error) {
@@ -33,20 +38,27 @@ const AllQuestion = () => {
       }
       setMessage("Error happened while Get Data, please try again");
     }
-    // useEffect(() => {
-    //   getAllQuestions();
-    // }, []);
-    return (
-      <>
-        <div className="Form">
-          {question &&
-            question.map((element) => {
-             return <p> element.question </p>;
-            })}
-        </div>
-      </>
-    );
   };
+  return (
+    <>
+      <div className="Form">
+        {question &&
+          question.map((element) => {
+            console.log(element);
+            return (
+              <>
+                {" "}
+                <p> {element.title} </p>
+                <p> {element.question} </p>;
+                <input placeholder="write answer" />
+                <button>add answer</button>
+              </>
+            );
+          })}
+      </div>
+      <p>{message}</p>
+    </>
+  );
 };
 
 export default AllQuestion;
